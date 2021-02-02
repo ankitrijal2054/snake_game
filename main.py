3a9b3a29c46af4853cb848de9e5d3bd6b42dc4b0
ankitrijal2054 <ankitrijal2054@gmail.com>
2021-02-02 10:47:08 -0600

    snake added

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from turtle import Screen, Turtle
 
 screen = Screen()
-screen.setup(width=600, height=400)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+pos = 0
+snake = []
 
-
+for _ in range(0,3):
+    tim = Turtle(shape = "square")
+    tim.color("white")
+    tim.setx(pos)
+    snake.append(tim)
+    pos -= 20
 
 
 
