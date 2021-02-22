e549b2768553ef0ffff2b7e48e8b594c54495342
ankitrijal2054 <ankitrijal2054@gmail.com>
2021-02-22 14:19:53 -0600

    highscore file created

--- scoreboard.py
+++ scoreboard.py
@@ -10,25 +10,32 @@ class Scoreboard(Turtle):
         self.speed("fastest")
         self.color("white")
         self.score = 0
-        self.high_score = 0
+        #self.high_score = 0
         self.display()
         self.play_again = True
 
     def display(self):
         self.clear()
         self.goto(0,270)
-        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.read_file()}", move=False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.display()
 
     def check_high_score(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
+        if self.score > int(self.read_file()):
+            self.write_file(self.score)
 
     def reset_game(self):
         self.check_high_score()
         self.score = 0
         self.display()
 
+    def read_file(self):
+        with open("highscore.txt") as file:
+            return file.read()
+
+    def write_file(self,score):
+        with open("highscore.txt", mode='w') as file:
+            file.write(str(score))
